76e2467a19853391610e8dbb951ecf66632b407e
Niekvandam <niekvandam1@outlook.com>
2019-08-06 20:16:21 +0200

    Initial commit

---
+++ Bot.py
@@ -0,0 +1,66 @@
+# -----------------------------------------------------------------------------
+import asyncio
+import configparser
+import json
+import os
+import sys
+import time
+from datetime import datetime
+from twisted.internet import task, reactor
+from pprint import pprint
+
+import ccxt
+
+import market
+# -----------------------------------------------------------------------------
+
+this_folder = os.path.dirname(os.path.abspath(__file__))
+root_folder = os.path.dirname(os.path.dirname(this_folder))
+sys.path.append(root_folder + '/python')
+sys.path.append(this_folder)
+
+# -----------------------------------------------------------------------------
+
+TIMEOUT = 60.0
+REQUIRED_CANDLES = 2
+SYMBOL = "BTC/EUR"
+
+
+# -----------------------------------------------------------------------------
+
+market_opens = []
+market_closes = []
+
+
+# -----------------------------------------------------------------------------
+
+long = True
+
+
+def wait_full_minute():
+    t = datetime.utcnow()
+    sleeptime = 60 - (t.second + t.microsecond / 1000000.0)
+    print("waiting for {} seconds until {}...".format(sleeptime, datetime.utcnow().minute))
+    time.sleep(sleeptime)
+
+
+def long_or_short():
+    global long
+    market_closes.append(market.get_market_close(SYMBOL))
+    market_opens.append(market.get_new_open(market_closes[-1]))
+    if len(market_closes) > REQUIRED_CANDLES:
+        if market_closes[-1] > market_opens[-1] > market_closes[-2] and long is False:
+            print("GOING LONG! at price {} || {}".format(market_opens[-1], datetime.now()))
+            long = True
+        elif market_closes[-1] < market_opens[-1] < market_closes[-2] and long is True:
+            print("GOING SHORT! at price {} || {}".format(market_opens[-1], datetime.now()))
+            long = False
+        else:
+            print("IDLE || {}".format(datetime.now()))
+
+
+while datetime.utcnow().minute != 9:
+    wait_full_minute()
+
+l = task.LoopingCall(long_or_short())
+l.start(180)
---
+++ market.py
@@ -0,0 +1,60 @@
+# -----------------------------------------------------------------------------
+
+import configparser
+import datetime
+import json
+import time
+
+import ccxt
+
+# -----------------------------------------------------------------------------
+
+config = configparser.ConfigParser()
+config.read("settings.SECRET.ini")
+
+# ----------------------------------------------------------------------------
+
+symbol = 'BTC/EUR'
+# TODO fill lists with corresponding data
+marketopens = []
+marketcloses = []
+
+# ----------------------------------------------------------------------------
+
+
+def get_exchange_ticker(symbol):
+    exchange = ccxt.coinbasepro({
+        'apiKey': config["coinbase"]["apiKey"],
+        'password': config["coinbase"]["password"],
+        'secret': config["coinbase"]["secret"],
+        'verbose': False,  # False -> no HTTP log
+        'enableRateLimit': True
+    })
+    ticker = exchange.fetch_ticker(symbol)
+    return ticker
+
+
+# TODO REPEATING CODE
+def get_market_open(symbol):
+    ticker = get_exchange_ticker("BTC/EUR")
+    current_market_open = json.dumps(ticker["info"]["price"])
+    # print("Market open {} || {}".format(current_market_open, datetime.datetime.now()))
+    return current_market_open
+
+
+def get_market_close(symbol):
+    ticker = get_exchange_ticker("BTC/EUR")
+    previous_close = json.dumps(ticker["info"]["price"])
+    # print("Market close {} || {}".format(previous_close, datetime.datetime.now()))
+    return previous_close
+
+
+def get_new_open(previous_close):
+    current_open = previous_close
+    while previous_close == current_open:
+        time.sleep(20)
+        ticker = get_exchange_ticker("BTC/EUR")
+        current_open = json.dumps(ticker["info"]["price"])
+    # print("New market open {} || {} ".format(current_open, datetime.datetime.now()))
+    print("Last price: {} || Current Price: {}".format(previous_close, current_open))
+    return current_open
